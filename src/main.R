db2634093c00e12f7b7a09e1f3c286adfaf3fcac
johnathanlo <49450170+johnathanlo@users.noreply.github.com>
2020-03-18 17:12:28 -0500

    Added function for making histograms of lateness

--- src/main.R
+++ src/main.R
@@ -1,4 +1,5 @@
-FlightDelays <- read.csv("FlightDelays.csv")
+###SET WD to Parent folder!###
+FlightDelays <- read.csv("data/FlightDelays.csv")
 FlightDelays$YEAR <- as.factor(FlightDelays$YEAR)
 FlightDelays$QUARTER <- as.factor(FlightDelays$QUARTER)
 FlightDelays$MONTH <- as.factor(FlightDelays$MONTH)
@@ -7,8 +8,15 @@ FlightDelays$DAY_OF_WEEK <- as.factor(FlightDelays$DAY_OF_WEEK)
 FlightDelays$ARR_DELAY[is.na(FlightDelays$ARR_DELAY)]<- 0
 FlightDelays$LATE_ARR <- ifelse(FlightDelays$ARR_DELAY>=0, FlightDelays$ARR_DELAY, 0)
 
-require(dplyr);require(dummies); require(fastDummies)
+require(dplyr);require(dummies); require(fastDummies); require(ggplot2)
 set.seed(0)
 FlightDelays05 <- sample_frac(FlightDelays, .05)
 FlightDelays05 <- dummy_cols(FlightDelays05)
 
+######Histogram of Late Arrivals######
+source("src/MakeLateArrHist.R")
+LateArrHist <- MakeLateArrHist(FlightDelays05 = FlightDelays05)
+plot(LateArrHist)
+######Histogram of Late Arrivals######
+
+######Inflated Poisson Regression######
