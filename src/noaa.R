a48bd7070929dc9233943b8a8cad7c0d1ee7dec3
isaacke9 <isaacke9@gmail.com>
2020-03-25 23:57:51 -0500

    stations.RData with running stations with atleast 2018-2019 data

--- src/noaa.R
+++ src/noaa.R
@@ -7,4 +7,7 @@ weather = meteo_pull_monitors(monitors, date_min = "2018-01-01", date_max="2019-
 names(airport_coords)[1] = "id"
 names(airport_coords)[2] = "latitude"
 names(airport_coords)[3] = "longitude"
-meteo_nearby_stations(airport_coords, lat_colname = "latitude", lon_colname="longitude" )
+mateo_stations = meteo_nearby_stations(airport_coords, lat_colname = "latitude", lon_colname="longitude" )
+
+monitors = as.vector(meteo_stations_trimmed$ID)
+weather = meteo_pull_monitors(monitors, date_min = "2018-01-01", date_max="2019-12-31", var=c("all"))
