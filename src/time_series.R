77c6cfb88b4fcd60885ffafc1cab74bf645d3cef
isaacke9 <isaacke9@gmail.com>
2020-04-02 00:10:22 -0500

    better auto.arima (hourly, weekly)

--- src/time_series.R
+++ src/time_series.R
@@ -5,12 +5,32 @@ require(zoo)
 require(quantmod)
 require(tidyquant)
 require(tseries)
+require(ggplot)
 
-##try to get residuals
+dshw()
+msts()
+#seasonal.periods=c(19,19*7)  for hourly and weekly
 
-timeseries_lm = lm(ARR_DELAY ~. -Route, data = sample_frac(FlightDelays_RF,0.05))
 
+time_series_weekly = group_by(FlightDelays, FL_DATE, DEP_TIME_BLK) %>% summarise(median_hourly_delay = median(ARR_DELAY, na.rm=TRUE))
 
+hourly_delay_ts = ts(time_series_weekly$median_hourly_delay, deltat = 1/19)
+autoplot(hourly_delay_ts)
+decompose(hourly_delay_ts)
+plot(decompose(hourly_delay_ts))
+
+arima_fit_hourly = auto.arima(hourly_delay_ts, D=1)
+summary(arima_fit_hourly)
+
+#findfrequency(daily_delay_ts)
+forecast_hourly = forecast(arima_fit_hourly, h=1596)
+plot(forecast_hourly[])
+
+g2 <- autoplot(forecast_hourly) + 
+  ggtitle("Hourly forecast") + 
+  ylab("y") +
+  coord_cartesian(xlim = c(545, 640))
+plot(g2)
 
 
 
@@ -18,7 +38,7 @@ timeseries_lm = lm(ARR_DELAY ~. -Route, data = sample_frac(FlightDelays_RF,0.05)
 median_daily_delay = group_by(FlightDelays, FL_DATE) %>% summarise(median_daily_delay = median(ARR_DELAY, na.rm=TRUE))
 
 median_daily_delay = rbind(median_daily_delay, median_daily_delay)
-daily_delay_ts = ts(median_daily_delay$median_daily_delay, deltat=1/365)
+daily_delay_ts = ts(median_daily_delay$median_daily_delay, deltat=1/7)
 autoplot(daily_delay_ts)
 str(daily_delay_ts)
 decompose(daily_delay_ts)
@@ -27,11 +47,18 @@ plot(decompose(daily_delay_ts))
 par(mfrow=c(1,1))
 plot(daily_delay_ts)
 
-arima_fit_daily = auto.arima(daily_delay_ts, stepwise=TRUE)
+arima_fit_daily = auto.arima(daily_delay_ts, D=2)
 summary(arima_fit_daily)
 
-forecast = forecast(arima_fit_daily, h =200)
+findfrequency(daily_delay_ts)
+forecast = forecast(arima_fit_daily, h=12)
 plot(forecast)
+
+g3 <- autoplot(forecast) + 
+  ggtitle("Weekly forecast") + 
+  ylab("y") +
+  coord_cartesian(xlim = c(75, 82))
+plot(g3)
 
 ###### backup: exponential smoothing (assuming no seasonality)
 Delay.Forecasts = HoltWinters(median_daily_delay, beta=FALSE, gamma = FALSE)
@@ -42,9 +69,10 @@ Delay.Forecasts$SSE
 
 
 ## TIME SERIES FOR AVG DAILY DELAY
-avg_monthly_delay = group_by(FlightDelays, MONTH) %>% summarise(avg_daily_delay = mean(ARR_DELAY, na.rm=TRUE))
+avg_monthly_delay = group_by(FlightDelays, MONTH) %>% summarise(avg_monthly_delay = mean(ARR_DELAY, na.rm=TRUE))
 
-monthly_delay_ts = ts(avg_monthly_delay$avg_daily_delay)
+avg_monthly_delay = rbind(avg_monthly_delay, avg_monthly_delay)
+monthly_delay_ts = ts(avg_monthly_delay$avg_monthly_delay)
 str(monthly_delay_ts)
 decompose(monthly_delay_ts, c("additive", "multiplicative"))
 
