98846a16303a6bbd12919bed0f5f969112e34e0f
isaacke9 <isaacke9@gmail.com>
2020-04-10 23:50:17 -0500

    function for OLS estimates

--- src/residuals.R
+++ src/residuals.R
@@ -4,31 +4,60 @@ require(ggally)
 require(psych)
 require(leaps)
 require(olsrr)
+require(broom)
+
+load("data/Rdata/arima_fit_hourly.RData")
+load("data/Rdata/arima_fit_weekly.RData")
+load("data/Rdata/FlightDelaysBootstrap.RData")
 
 FlightDelaysBootstrap = ungroup(FlightDelaysBootstrap)
 FlightDelaysBootstrap$arima_weekly_res = as.vector(arima_fit_weekly$residuals)
 FlightDelaysBootstrap$arima_hourly_res = as.vector(arima_fit_hourly$residuals)
 
+## Generate OLS model 
+# data = data frame with covariates
+# n = number of models to be fit
+# sample.size = size of each subset
+# returns list of 2: coef_samples (all estimates over n subset), coef_avg (avg of coefficients)
+generate_OLS_coef <- function(data, n, sample.size) {
+  for (i in 1:n) {
+    set.seed(2020+i)
+    subset_residuals = sample_n(data, sample.size)
+    wts = c(sqrt((subset_residuals$prcp)+1))
+    lm_res_weekly = lm(arima_weekly_res ~ . -ARR_DELAY -Route -YEAR -QUARTER -MONTH -DAY_OF_MONTH -DAY_OF_WEEK -DEP_TIME_BLK -ARR_TIME_BLK, weights = wts, data = subset_residuals)
+    #create data frame
+    if (i==1) {
+      coefficients = as.data.frame(tidy(lm_res_weekly)[,1:2])
+      names(coefficients)[2] = "estimate_1"
+    }
+    #update data frame
+    else {
+      col_name <- paste("estimate_", i, sep = "")
+      coefficients = merge(coefficients, as.data.frame(tidy(lm_res_weekly)[,1:2]), by = "term", all.x=TRUE)
+      names(coefficients)[i+1]=col_name
+    }
+  }
+  coefficient_avg =  rowwise(coefficients) %>% 
+    mutate(coef_avg=mean(c(estimate_1,estimate_2, estimate_3, estimate_4, estimate_5))) %>%
+      select(term, coef_avg) %>%
+        rename(predictor = term)
+  return(list(coef_samples = as.data.frame(coefficients), coef_avg = as.data.frame(coefficient_avg)))
+}
 
-str(FlightDelaysBootstrap)
-subset_residuals = sample_n(FlightDelaysBootstrap, 50000)
-wts = c(sqrt((subset_residuals$prcp)+1))
-lm_res_weekly = lm(arima_weekly_res ~ . -ARR_DELAY -Route -YEAR -QUARTER -MONTH -DAY_OF_MONTH -DAY_OF_WEEK -DEP_TIME_BLK -ARR_TIME_BLK , weights = wts, data = subset_residuals)
-summary(lm_res_weekly)
-plot(lm_res_weekly)
+# For our additive model 
+OLS_estimates = generate_OLS_coef(FlightDelaysBootstrap,5, 50000)
 
+linear_final = OLS_estimates$coef_avg
+save(list = c("linear_final"), file = "data/Rdata/linear_final.RData")
+
+##############################################################################################
+## see if we should fit another model on the residuals of the residuals
 subset_residuals$res_of_res = as.vector(lm_res_weekly$residuals)
 lm_res_of_res = lm(res_of_res ~ .  -ARR_DELAY -Route -YEAR -QUARTER -MONTH -DAY_OF_MONTH -DAY_OF_WEEK -DEP_TIME_BLK -ARR_TIME_BLK -arima_weekly_res -arima_hourly_res, data = subset_residuals)
-summary(lm_res_of_res)
+summary(lm_res_of_res) ## we see doing this doesn't explain any more variation in the data, so we stop 
 plot(lm_res_of_res)
 
-### Stepwise variable selection
-predictors = (pairs[,-c(55)])
-response = as.vector(pairs[,c(55)])
-leaps = regsubsets(arima_weekly_res ~ . -ARR_DELAY, data = pairs, nbest = 10, method = "forward")
-which = as.data.frame(summary(leaps)$which)
-
-
+##############################################################################################
 ### see transformations of variables
 pairs = select(subset_residuals, -c(ORIGIN, DEST, YEAR, QUARTER, MONTH, DAY_OF_MONTH, DAY_OF_WEEK, CARRIER, Route, DEP_TIME_BLK, ARR_TIME_BLK, carrier_lg, carrier_low))
 
