cd22085a3009d4da3ef3ec49f8ff345cb8a89935
johnathanlo <49450170+johnathanlo@users.noreply.github.com>
2020-04-12 11:25:30 -0500

    sdad

--- src/DynamicLinearModel.R
+++ src/DynamicLinearModel.R
@@ -1,8 +1,9 @@
 PredictDelays <- function(obs){
-  load("data/arima_fit_hourly.RData")
-  load("data/arima_fit.RData")
-  load("data/linear_final.RData")
-  
+  #########Function needs the following data loaded to work
+  ##load("data/arima_fit_hourly.RData")
+  ##load("data/arima_fit.RData")
+  ##load("data/Rdata/residual_linear_model.RData")
+  days = 0
   if(obs$MONTH == 8){
     days = 31
   }else if(obs$MONTH == 9){
@@ -10,15 +11,17 @@ PredictDelays <- function(obs){
   }else if(obs$MONTH==10){
     days = 92
   }
-  days = days + obs$DAY_OF_MONTH
+  days = days + as.numeric(obs$DAY_OF_MONTH)
   steps = days*19+as.numeric(obs$DEP_TIME_BLK)
   
+  obs$MONTH <- as.factor(obs$MONTH)
+  obs$QUARTER<- as.factor(obs$QUARTER)
   hresid<-predict(arima_fit_hourly, n.ahead = steps)
-  obs$arima_hourly_res = resid
+  obs$arima_hourly_res = hresid$pred[steps]
   
   eta <- predict(arima_fit, n.ahead = steps)
-  
-  lin <- predict(linear_final,newdata = obs )
+  obs$arima_weekly_res = eta$pred[steps]
+  lin <- predict(lm_res_weekly,newdata = obs )
   
   return (eta+lin)
 }
